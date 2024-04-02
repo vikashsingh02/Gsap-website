9947db451e0306268498160135f81620734f6f79
Vikash singh <107889207+vikashsingh02@users.noreply.github.com>
2024-04-02 16:08:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var videocon = document.querySelector(".video-container");
+var cursor = document.querySelector(".cursor");
+var playbtn = document.querySelector(".play");
+videocon.addEventListener("mouseenter", function () {
+  gsap.to(playbtn, {
+    scale: 1,
+    opacity: 0.8,
+  });
+
+});
+videocon.addEventListener("mouseleave", function () {
+  gsap.to(playbtn, {
+    scale: 0,
+    opacity: 0,
+  });
+});
+videocon.addEventListener("mousemove", function(dets) {
+    gsap.to(playbtn, {
+      left:dets.x,
+      top:dets.y
+    });
+  });
+
+document.addEventListener("mousemove",function(dets){
+ gsap.to(cursor, {
+      left:dets.x,
+      top:dets.y
+    });
+})
